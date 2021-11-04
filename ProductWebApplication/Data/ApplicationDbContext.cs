6b16306e45275a9ad06d57f122a060a0609fb5be
fodhil1 <fodhilzidane@gmail.com>
2021-11-04 13:40:32 -0400

    Product App

---
+++ ProductWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApplication.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ProductWebApplication/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApplication.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set;}
+        public int price { get; set; }
+
+    }
+}
